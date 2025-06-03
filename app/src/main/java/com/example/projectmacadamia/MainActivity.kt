401c876980c9e225c59a2fde66a88389c579f892
Irene-lc <irene.landivar13@gmail.com>
2025-06-03 18:24:52 -0400

    Cambios en los botones

--- app/src/main/java/com/example/projectmacadamia/MainActivity.kt
+++ app/src/main/java/com/example/projectmacadamia/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.projectmacadamia
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +12,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+        val orderNowButton = findViewById<Button>(R.id.orderNow)
+        orderNowButton.setOnClickListener(View.OnClickListener {view : View? ->
+            val intent = Intent(this@MainActivity, elegir::class.java)
+            startActivity(intent)
+        })
     }
 }
--- app/src/main/java/com/example/projectmacadamia/elegir.kt
+++ app/src/main/java/com/example/projectmacadamia/elegir.kt
@@ -1,10 +1,14 @@
 package com.example.projectmacadamia
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.projectmacadamia.MainActivity
 
 class elegir : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,15 @@ class elegir : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_elegir)
 
+        val siginButton = findViewById<Button>(R.id.botonRegister)
+        siginButton.setOnClickListener(View.OnClickListener {view : View? ->
+            val intent = Intent(this@elegir, activity_signin::class.java)
+            startActivity(intent)
+        })
+        val loginButton = findViewById<Button>(R.id.botonLogIn)
+        loginButton.setOnClickListener(View.OnClickListener {view : View? ->
+            val intent = Intent(this@elegir, activiy_login::class.java)
+            startActivity(intent)
+        })
     }
 }

9113d3023d440ad55bd2a5463946b86ee9c0cdd9
Irene-lc <irene.landivar13@gmail.com>
2025-06-09 08:12:07 -0400

    toolbar

--- app/src/main/java/com/example/projectmacadamia/activity_profile.kt
+++ app/src/main/java/com/example/projectmacadamia/activity_profile.kt
@@ -15,13 +15,6 @@ class activity_profile : AppCompatActivity() {
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
-
-        val builder = AppBarConfiguration.Builder(navController.graph)
-        val appBarConfiguration = builder.build()
-        toolbar.setupWithNavController(navController, appBarConfiguration)
 
     }
 }

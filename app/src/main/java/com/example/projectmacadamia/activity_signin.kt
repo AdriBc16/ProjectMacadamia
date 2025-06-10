e55cf6c5db1d73dd631659a69ff3c1f893fa8377
Irene-lc <irene.landivar13@gmail.com>
2025-06-10 12:42:30 -0400

    frames, boton login y sigin llevan a perfil, scroll home

--- app/src/main/java/com/example/projectmacadamia/activity_signin.kt
+++ app/src/main/java/com/example/projectmacadamia/activity_signin.kt
@@ -9,12 +9,17 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projectmacadamia.MainActivity
+import com.example.projectmacadamia.activiy_login
 
 class activity_signin : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_signin)
-
+        val botonSignin = findViewById<Button>(R.id.button_SignIn)
+        botonSignin.setOnClickListener {
+            val intent = Intent(this@activity_signin, PadreActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
